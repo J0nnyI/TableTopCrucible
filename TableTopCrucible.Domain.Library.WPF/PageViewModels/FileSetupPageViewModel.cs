ee836798b7cd4f0c1922d3d9a06318de3a39820d
Marcel Rehkemper <marcel.rehkemper@gmx.de>
2020-10-08 00:41:35 +0200

    file setup page cleanup

--- TableTopCrucible.Domain.Library.WPF/PageViewModels/FileSetupPageViewModel.cs
+++ TableTopCrucible.Domain.Library.WPF/PageViewModels/FileSetupPageViewModel.cs
@@ -16,16 +16,12 @@ namespace TableTopCrucible.Domain.Library.WPF.PageViewModels
         private readonly ISaveService saveService;
 
         public DirectoryListViewModel DirList { get; }
-        public FileListViewModel FileList { get; }
-        public ItemListViewModel ItemList { get; }
         public NotificationCenterViewModel NotificationCenter { get; }
         public ICommand OpenFile { get; }
         public ICommand SaveFile { get; }
 
         public FileSetupPageViewModel(
             DirectoryListViewModel dirList,
-            FileListViewModel fileList,
-            ItemListViewModel itemList,
             NotificationCenterViewModel notificationCenter,
             OpenFileDialogCommand openFile,
             SaveFileDialogCommand saveFile,
@@ -33,8 +29,6 @@ namespace TableTopCrucible.Domain.Library.WPF.PageViewModels
             ) : base("File Setup", PackIconKind.File)
         {
             this.DirList = dirList;
-            this.FileList = fileList;
-            this.ItemList = itemList;
             this.NotificationCenter = notificationCenter;
             OpenFile = openFile;
             SaveFile = saveFile;
@@ -43,6 +37,6 @@ namespace TableTopCrucible.Domain.Library.WPF.PageViewModels
         }
 
         public Action<string> OpenFileAction => saveService.Load;
-        public Action<string> SaveFileAction;
+        public Action<string> SaveFileAction { get; }
     }
 }
